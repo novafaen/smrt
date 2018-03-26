7926f6dde08648b8d91423d0b3249438ce51f4fb
Kristoffer Nilsson <kristoffer@novafaen.se>
2018-03-26 20:48:35 +0200

    Initial commit

---
+++ smrt/smrt.py
@@ -0,0 +1,66 @@
+import logging
+import time
+
+from flask import Flask, request, make_response, jsonify
+
+logging.basicConfig(
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.DEBUG
+)
+
+
+class SMRT(Flask):
+    def __init__(self, *args, **kwargs):
+        super(SMRT, self).__init__(*args, **kwargs)
+
+        self.app = None
+        self.ping = 'pong'
+
+        # scan for packages
+
+    def version(self):
+        body = {
+            'smrt_version': 'test',
+            'time': time.time()
+        }
+
+        # add app version information
+        if self.app is not None:
+            app_version = self.app.version()
+            body['app_version'] = app_version['app_version']
+
+        return body
+
+
+# create app
+app = SMRT(__name__)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    body = {
+        'status': 'NotFound',
+        'code': 404,
+        'message': 'Requested URL (%s) does not exist.' % request.path,
+        'derived_message': str(error)
+    }
+    response = make_response(jsonify(body), 404)
+    response.headers['Content-Type'] = 'application/se.novafaen.smrt.error.v1+json'
+    return response
+
+
+@app.route('/version')
+def version():
+    body = app.version()
+    response = make_response(jsonify(body), 200)
+    response.headers['Content-Type'] = 'application/se.novafaen.smrt.version.v1+json'
+    return response
+
+
+@app.route('/register/<string:service>/<string:identifier>')
+def register(service, identifier):
+    logging.debug('received register request %s:%s', service, identifier)
+
+    response = make_response('', 204)
+    return response
