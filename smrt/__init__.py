56e3c26e845888bb8cf1d233a80362be844c8162
Kristoffer Nilsson-Chavez <kristoffer@novafaen.se>
2020-02-01 10:20:48 +0100

    flask naming convention app.py

--- smrt/__init__.py
+++ smrt/__init__.py
@@ -13,7 +13,7 @@ Exports:
 - `jsonify` Flask jsonify function.
 """
 
-from .smrt import app, smrt, request, make_response, jsonify, log, \
+from .app import app, smrt, request, make_response, jsonify, log, \
                   ResouceNotFound
 from .smrtapp import SMRTApp
 from .schemas import read_schema, validate_json
